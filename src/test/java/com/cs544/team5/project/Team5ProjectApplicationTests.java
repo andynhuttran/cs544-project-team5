81f3ff7dff3e77e28ecd662bbb559733c2bc3c9a
colenhuttran <tranthenhut@gmail.com>
2021-06-02 16:02:14 -0500

    first commition

---
+++ src/test/java/com/cs544/team5/project/Team5ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs544.team5.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team5ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
