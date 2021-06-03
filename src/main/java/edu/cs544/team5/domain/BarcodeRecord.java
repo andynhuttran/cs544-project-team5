ddbad90d1867ee34020d7973ae25d299bf14fad3
Abdulaziz Ali <rediabdulaziz@gmail.com>
2021-06-03 18:40:12 -0500

    all domain entities

---
+++ src/main/java/edu/cs544/team5/domain/BarcodeRecord.java
@@ -0,0 +1,22 @@
+package edu.cs544.team5.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Setter
+@Getter
+public class BarcodeRecord {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private LocalDateTime attendance;
+    @ManyToOne
+    @JoinColumn(name = "barcode")
+    private Student student;
+    @ManyToOne
+    private ClassSession classSession;
+}
---
+++ src/main/java/edu/cs544/team5/domain/ClassSession.java
@@ -0,0 +1,24 @@
+package edu.cs544.team5.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+
+@Entity
+@Setter
+@Getter
+public class ClassSession {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private LocalDate date;
+    @ManyToOne
+    private Timeslot timeslot;
+    @ManyToOne
+    private CourseOffering courseOffering;
+    @ManyToOne
+    private Location location;
+}
