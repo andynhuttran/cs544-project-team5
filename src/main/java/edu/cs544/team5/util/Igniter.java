8e75d569ef9636b26a5fe8030b3dd8100e47c73f
Abdulaziz Ali <rediabdulaziz@gmail.com>
2021-06-08 21:47:56 -0500

    add super admin

--- src/main/java/edu/cs544/team5/util/Igniter.java
+++ src/main/java/edu/cs544/team5/util/Igniter.java
@@ -9,21 +9,22 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import static edu.cs544.team5.domain.RoleType.*;
 
 @RequiredArgsConstructor
 @Component
+
 public class Igniter implements CommandLineRunner {
     private final PersonRepository personRepository;
     private final RoleRepository roleRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Override
-    public void run(String... args) throws Exception {
-        List.of(new Role(ADMIN), new Role(STUDENT), new Role(FACULTY), new Role(PERSONNEL)).forEach(
-                roleRepository::save
-        );
+    @Transactional
+    public void run(String... args) {
+        List<Role> roles = List.of(new Role(ADMIN), new Role(STUDENT), new Role(FACULTY), new Role(PERSONNEL));
 
         Person admin = new Person();
         admin.setFirstName("Admin");
@@ -31,6 +32,10 @@ public class Igniter implements CommandLineRunner {
         admin.setUsername("admin");
         admin.setActive(true);
         admin.setPassword(bCryptPasswordEncoder.encode("admin"));
+        admin.addRole(roles.get(0));
+        roles.forEach(
+                roleRepository::save
+        );
         personRepository.save(admin);
     }
 }
