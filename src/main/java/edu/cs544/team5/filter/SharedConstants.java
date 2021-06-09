02e1d6285dc45bd4ca62a5813cbb3bace9f5a609
Abdulaziz Ali <rediabdulaziz@gmail.com>
2021-06-08 22:52:56 -0500

    update consts

--- src/main/java/edu/cs544/team5/filter/SharedConstants.java
+++ src/main/java/edu/cs544/team5/filter/SharedConstants.java
@@ -2,7 +2,7 @@ package edu.cs544.team5.filter;
 
 public class SharedConstants {
     public static final String SECRET = "noop";
-    public static final long EXPIRATION_TIME = 300000; // 1 day
+    public static final long EXPIRATION_TIME = 300000;
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
 }
