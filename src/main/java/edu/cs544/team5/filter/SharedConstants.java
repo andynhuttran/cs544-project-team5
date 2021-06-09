4edd3e07b16a71ea48990de6c525cf965c770430
Abdulaziz Ali <rediabdulaziz@gmail.com>
2021-06-08 22:51:34 -0500

    fix yml conf values

---
+++ src/main/java/edu/cs544/team5/filter/SharedConstants.java
@@ -0,0 +1,8 @@
+package edu.cs544.team5.filter;
+
+public class SharedConstants {
+    public static final String SECRET = "noop";
+    public static final long EXPIRATION_TIME = 300000; // 1 day
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String HEADER_STRING = "Authorization";
+}
